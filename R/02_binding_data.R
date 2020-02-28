64b4af047d1c87a49b7aae86cf2de727a5beb7b6
saramortara <saramortara@gmail.com>
2020-02-28 09:53:00 -0300

    renaming script2

--- R/02_binding_data.R
+++ R/02_binding_data.R
@@ -1,23 +1,15 @@
-# Script to analyze data of female representativity in science
-# reading additional information of female researchers
+# Script to read additional information of female researchers
 # binds both data and makes graphics
 # by Sara Mortara, first version 2020-02-27
 
 ## loading packages
-library(ggplot2)
 library(dplyr)
-library(stringr)
-library(wesanderson)
-
-## color pallete for graphics
-cores <-  wes_palette("Darjeeling1", 2)[2:1]
-ver <- rgb(255, 0, 0, maxColorValue = 255, alpha = 150)
-cin <- rgb(106, 126, 133, maxColorValue = 255)
 
 ## loading files
 fa <- read.csv("outputs/female_area_2.csv")
 fs <- read.csv("outputs/female_science_2.csv")
 
+# 1. juntando os dados ####
 # juntando as duas tabelas, e removendo as duplicadas
 fall <- bind_rows(fa, fs, .id = 'id') %>%
   .[!duplicated(.[,-1]), ]
@@ -27,5 +19,7 @@ fall$id <- ifelse(fall$id == 1, "area", "geral")
 head(fall)
 
 # escrevendo o output
-write.csv(fall, "outputs/02_all_women.csv",
-          row.names = FALSE)
+# write.csv(fall, "outputs/02_all_women.csv",
+#           row.names = FALSE)
+
+
